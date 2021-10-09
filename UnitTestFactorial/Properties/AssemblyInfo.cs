71d9f832a9de55c3876dd0d4a68712092c2fd259
Anicet <tghil7@aol.com>
2021-10-09 11:42:44 -0500

    Add project files.

---
+++ UnitTestFactorial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFactorial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFactorial")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93b5e627-9863-4cc2-a807-f07ef2d561a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestFactorial/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestFactorial
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        int Factorial(int n)
+        {
+            if (n <= -1 || n >= 13)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+          
+            int result = 1;
+           /* for (int i= n ; i > 1; i--)
+            {
+                //result *= i;
+                
+                
+            }*/
+
+            while (n > 1)
+            {
+                result *= n;
+                n--;
+            }
+
+            return result;
+            
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodMinValue() // Input: MinValue, Output: error
+        {
+            Factorial(int.MinValue);
+        }
+
+      
+
+
+
+        [TestMethod]
+        public void TestMethod0() // Input: 0, Output: 1
+        {
+
+            Assert.AreEqual(1, Factorial(0));
+        }
+        [TestMethod]
+        public void TestMethod5() // Input: 5, Output: 120
+        {
+            Assert.AreEqual(120, Factorial(5));
+        }
+
+        [TestMethod]
+        public void TestMethod10() // Input: 10, Output: 3,628,800
+        {
+            Assert.AreEqual(3628800, Factorial(10));
+        }
+
+        [TestMethod]
+        public void TestMethodMaxValue10exp12() // Input: 12, Output: 479001600
+        {
+            Assert.AreEqual(479001600, Factorial(12));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodMaxValue10exp13() // Input: 13, Output: ERROR
+        {
+          Factorial(13);
+        }
+
+
+    }
+}
